e00f1e2a6b9dff6a4aad1cfc3c4d2b25a05be65b
liubavabarsegian <lyubahemmo@mail.ru>
2023-04-30 21:56:40 +0300

    deleted old files

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root 'train#train', dog_picture: "standing.png", person_picture:"neutral.png"
-  # post '/train/read', dog_picture: "standing.png", person_picture:"neutral.png"
-  post 'train/refresh', dog_picture: "standing.png", person_picture:"neutral.png"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
--- test/controllers/train_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class TrainControllerTest < ActionDispatch::IntegrationTest
-  test "should get train" do
-    get train_train_url
-    assert_response :success
-  end
-end
